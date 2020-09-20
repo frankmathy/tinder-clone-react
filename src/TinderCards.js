5b75dab57f9b67d0f3922b57ba7a9ee68a0ee767
Frank Mathy <frankmathy@gmail.com>
2020-09-20 13:04:39 +0200

    Deploy to firebase

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TinderCards.css';
 import TinderCard from 'react-tinder-card';
+import axios from './axios';
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: 'Elon Musk',
-      url:
-        'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg'
-    },
-    {
-      name: 'Jeff Bezos',
-      url:
-        'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg'
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/tinder/cards');
+      setPeople(req.data);
     }
-  ]);
+
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log('removing: ' + nameToDelete);
-    // setLastDirection(direction);
   };
 
   const outOfFrame = name => {
@@ -37,7 +35,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
